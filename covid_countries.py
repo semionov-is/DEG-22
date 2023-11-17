dae726de9a8d6f2db57a888f5b9553344fd31de1
semionov-is <107299818+semionov-is@users.noreply.github.com>
2023-11-17 16:02:24 +0300

    Add files via upload

---
+++ covid_countries.py
@@ -0,0 +1,57 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, desc, lag, rank
+from pyspark.sql.window import Window
+
+spark = SparkSession.builder.appName("Python Spark SQL COVID analysis").getOrCreate()
+
+df = spark.read.csv("owid-covid-data.csv", header=True, inferSchema=True)
+
+march_31_data = df.filter((col('date') == '2021-03-31'))
+march_31_data = march_31_data.withColumn("percentage_infected", (col("total_cases") / col("population")) * 100)
+result = march_31_data.select("iso_code", "location", "percentage_infected")
+
+result = result.orderBy("percentage_infected", ascending=False).limit(15)
+result = result.withColumnRenamed("location", "страна")
+result = result.withColumnRenamed("percentage_infected", "процент переболевших")
+
+print("15 стран с наибольшим процентом переболевших на 31.03.2021:")
+result.show()
+
+
+
+df_march = df.filter((df.date >= "2021-03-25") & (df.date <= "2021-03-31"))
+df_march = df_march.filter(~df_march.location.isin(["World", "Europe", "European Union", "Asia","North America",
+                                                    "South America", "Africa"]))
+top_10_countries = df_march.groupBy("date","location").sum("new_cases")
+top_10_countries = top_10_countries.orderBy(desc("sum(new_cases)")).limit(10)
+
+windowSpec = Window.partitionBy("date").orderBy(desc("sum(new_cases)"))
+top_10_countries = top_10_countries.withColumn("rank", rank().over(windowSpec))
+top_10_countries = top_10_countries.filter(col("rank") <= 10).drop("rank")
+
+top_10_countries = top_10_countries.withColumnRenamed("date", "число")
+top_10_countries = top_10_countries.withColumnRenamed("location", "страна")
+top_10_countries = top_10_countries.withColumnRenamed("sum(new_cases)", "количество новых случаев")
+
+print("Top 10 стран с максимальным зафиксированным кол-вом новых случаев за последнюю неделю марта (25-31.03) 2021:")
+top_10_countries.show()
+
+
+
+df_russia = df.filter((df.location == "Russia") & (df.date >= "2021-03-25") & (df.date <= "2021-03-31"))
+df_russia = df_russia.orderBy(df_russia.date)
+
+window = Window.orderBy(df_russia.date)
+df_russia = df_russia.withColumn("number_of_new_cases_yesterday", lag(col("new_cases"), 1).over(window))
+df_russia = df_russia.withColumn("delta", col("new_cases") - col("number_of_new_cases_yesterday"))
+df_russia = df_russia.select("date", "number_of_new_cases_yesterday", "new_cases", "delta")
+
+df_russia = df_russia.withColumnRenamed("date", "число")
+df_russia = df_russia.withColumnRenamed("number_of_new_cases_yesterday", "кол-во новых случаев вчера")
+df_russia = df_russia.withColumnRenamed("new_cases", "кол-во новых случаев сегодня")
+df_russia = df_russia.withColumnRenamed("delta", "дельта")
+
+print("изменение случаев относительно предыдущего дня в России за последнюю неделю марта (25-31.03) 2021:")
+df_russia.show()
+
+spark.stop()
